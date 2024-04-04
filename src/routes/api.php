de7891dcd826db77a1a335d936f88b6235966083
Antonio <antblajim@gmail.com>
2024-04-04 13:20:18 +0200

    Creacion-API -- La API funciona correctamente

---
+++ src/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function(){
+    Route::resource('usuarios','Api\V1\UsuariosController');
+
+});
